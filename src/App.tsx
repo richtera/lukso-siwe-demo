8de9552e59d3eba981b308a419c2a318a64649a3
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-10-28 19:25:47 -0400

    fix: Cleanup title

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,21 @@ export function App() {
       <nav className="flex justify-between items-center">
         <Web3Button />
       </nav>
-      <h1 className="text-lg">Login using SIWE with Universal Profile</h1>
+      <h1 className="text-lg">
+        Web3 Auth: Using Universal Profile and SIWE for Login
+      </h1>
       <h2 className="text-md">
+        This webpage shows several basic functions needed to use Universal
+        Profiles and SIWE for authentication, authorization and verification.
         Create signatures as JWT tokens, validate them from client to server and
         server to client.{' '}
-        {isConnected ? '' : 'Please connect the Universal Profile Extension.'}
+        {isConnected ? (
+          ''
+        ) : (
+          <span className="text-red bg-gray-500">
+            Please connect the Universal Profile Extension.
+          </span>
+        )}
       </h2>
 
       {isConnected && <SignMessage />}
